0550ea6e3ee6aa02fbfd7fddab69b80ecefb3826
nicolaics <nicolai.christian.s@gmail.com>
2024-08-28 01:13:17 +0900

    add types for supplier payload

--- BACKEND/types/supplier.go
+++ BACKEND/types/supplier.go
@@ -4,7 +4,17 @@ import (
 	"time"
 )
 
-type RegisterSupplier struct {
+type RegisterSupplierPayload struct {
+	Name                string `json:"name" validate:"required"`
+	Address             string `json:"address" validate:"required"`
+	CompanyPhoneNumber  string `json:"companyPhoneNumber" validate:"required"`
+	ContactPersonName   string `json:"contactPersonName"`
+	ContactPersonNumber string `json:"contactPersonNumber"`
+	Terms               string `json:"terms" validate:"required"`
+	VendorIsTaxable     bool   `json:"vendorIsTaxable" validate:"required"`
+}
+
+type DeleteSupplierPayload struct {
 	Name string `json:"name" validate:"required"`
 }
 
@@ -12,10 +22,19 @@ type SupplierStore interface {
 	GetSupplierByName(name string) (*Supplier, error)
 	GetSupplierByID(id int) (*Supplier, error)
 	CreateSupplier(Supplier) error
+	GetAllSuppliers() ([]Supplier, error)
+	DeleteSupplier(*Supplier) error
+	ModifySupplier(id int, newSupplierData Supplier) error
 }
 
 type Supplier struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"createdAt"`
+	ID                  int       `json:"id"`
+	Name                string    `json:"name"`
+	Address             string    `json:"address"`
+	CompanyPhoneNumber  string    `json:"companyPhoneNumber"`
+	ContactPersonName   string    `json:"contactPersonName"`
+	ContactPersonNumber string    `json:"contactPersonNumber"`
+	Terms               string    `json:"terms"`
+	VendorIsTaxable     bool      `json:"vendorIsTaxable"`
+	CreatedAt           time.Time `json:"createdAt"`
 }
