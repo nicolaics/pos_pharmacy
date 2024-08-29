c9d2acdb8a4a14e9216cf6a7e38312d266c5d4a9
nicolaics <nicolai.christian.s@gmail.com>
2024-08-29 13:41:13 +0900

    add verify token route

--- BACKEND/cmd/api/api.go
+++ BACKEND/cmd/api/api.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/nicolaics/pos_pharmacy/logger"
+	"github.com/nicolaics/pos_pharmacy/service/auth"
 	"github.com/nicolaics/pos_pharmacy/service/cashier"
 	"github.com/nicolaics/pos_pharmacy/service/customer"
 	"github.com/nicolaics/pos_pharmacy/service/supplier"
@@ -15,24 +16,26 @@ import (
 )
 
 type APIServer struct {
-	addr string
-	db *sql.DB
+	addr        string
+	db          *sql.DB
 	redisClient *redis.Client
 }
 
 func NewAPIServer(addr string, db *sql.DB, redisClient *redis.Client) *APIServer {
 	return &APIServer{
-		addr: addr,
-		db: db,
+		addr:        addr,
+		db:          db,
 		redisClient: redisClient,
 	}
 }
 
 func (s *APIServer) Run() error {
-	loggerVar:= log.New(os.Stdout, "", log.LstdFlags)
-	
+	loggerVar := log.New(os.Stdout, "", log.LstdFlags)
+
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	router.HandleFunc("/verify", auth.AuthenticationMiddleware(auth.AuthHandler))
 
 	cashierStore := cashier.NewStore(s.db, s.redisClient)
 	customerStore := customer.NewStore(s.db)
@@ -43,14 +46,14 @@ func (s *APIServer) Run() error {
 
 	customerHandler := customer.NewHandler(customerStore, cashierStore)
 	customerHandler.RegisterRoutes(subrouter)
-	
+
 	supplierHandler := supplier.NewHandler(supplierStore, cashierStore)
 	supplierHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening on: ", s.addr)
 
 	logMiddleware := logger.NewLogMiddleware(loggerVar)
-    router.Use(logMiddleware.Func())
+	router.Use(logMiddleware.Func())
 
 	return http.ListenAndServe(s.addr, router)
 }
