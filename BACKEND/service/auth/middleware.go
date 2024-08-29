63a4c63c3a5fdf24fc9c4268023b611e225bdb7e
nicolaics <nicolai.christian.s@gmail.com>
2024-08-29 13:43:59 +0900

    add authMiddleware

---
+++ BACKEND/service/auth/middleware.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// SRC: https://gist.github.com/AxelRHD/2344cc1105afc06723b363f21486dec8
+
+func AuthenticationMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := ValidateAccessToken(r)
+		if err != nil {
+			http.Error(w, "authentication error!", http.StatusForbidden)
+			log.Println("authentication error!")
+
+			return
+		}
+
+		next(w, r)
+	}
+}
+
+// restricted secret route
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "authorized")
+}
