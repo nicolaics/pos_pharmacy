66053711fe5fa3eb518e822cacfe01eb70f94206
nicolaics <nicolai.christian.s@gmail.com>
2024-08-31 20:55:24 +0900

    add purchase order invoice rotues

---
+++ BACKEND/service/poinvoice/routes.go
@@ -0,0 +1,402 @@
+package poinvoice
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+
+	"github.com/nicolaics/pos_pharmacy/types"
+	"github.com/nicolaics/pos_pharmacy/utils"
+)
+
+type Handler struct {
+	poInvoiceStore      types.PurchaseOrderInvoiceStore
+	cashierStore        types.CashierStore
+	supplierStore       types.SupplierStore
+	companyProfileStore types.CompanyProfileStore
+	medStore            types.MedicineStore
+	unitStore           types.UnitStore
+}
+
+func NewHandler(poInvoiceStore types.PurchaseOrderInvoiceStore, cashierStore types.CashierStore,
+	supplierStore types.SupplierStore, companyProfileStore types.CompanyProfileStore,
+	medStore types.MedicineStore, unitStore types.UnitStore) *Handler {
+	return &Handler{
+		poInvoiceStore:      poInvoiceStore,
+		cashierStore:        cashierStore,
+		supplierStore:       supplierStore,
+		companyProfileStore: companyProfileStore,
+		medStore:            medStore,
+	}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/invoice/purchase-order", h.handleNew).Methods(http.MethodPost)
+	router.HandleFunc("/invoice/purchase-order", h.handleGetPurchaseOrderInvoices).Methods(http.MethodGet)
+	router.HandleFunc("/invoice/purchase-order/items", h.handleGetPurchaseOrderItems).Methods(http.MethodGet)
+	router.HandleFunc("/invoice/purchase-order", h.handleDelete).Methods(http.MethodDelete)
+	router.HandleFunc("/invoice/purchase-order", h.handleModify).Methods(http.MethodPatch)
+
+	router.HandleFunc("/invoice/purchase-order", func(w http.ResponseWriter, r *http.Request) { utils.WriteJSONForOptions(w, http.StatusOK, nil) }).Methods(http.MethodOptions)
+	router.HandleFunc("/invoice/purchase-order/medicine-items", func(w http.ResponseWriter, r *http.Request) { utils.WriteJSONForOptions(w, http.StatusOK, nil) }).Methods(http.MethodOptions)
+}
+
+func (h *Handler) handleNew(w http.ResponseWriter, r *http.Request) {
+	// get JSON Payload
+	var payload types.NewPurchaseOrderInvoicePayload
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	// validate token
+	cashier, err := h.cashierStore.ValidateCashierToken(w, r, false)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("cashier token invalid"))
+		return
+	}
+
+	// check companyID
+	_, err = h.companyProfileStore.GetCompanyProfileByID(payload.CompanyID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("company id %d not found", payload.CompanyID))
+		return
+	}
+
+	// check supplierID
+	_, err = h.supplierStore.GetSupplierByID(payload.SupplierID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("supplier id %d not found", payload.SupplierID))
+		return
+	}
+
+	err = h.poInvoiceStore.CreatePurchaseOrderInvoice(types.PurchaseOrderInvoice{
+		Number:       payload.Number,
+		CompanyID:    payload.CompanyID,
+		SupplierID:   payload.SupplierID,
+		CashierID:    cashier.ID,
+		TotalItems:   payload.TotalItems,
+		InvoiceDate:  payload.InvoiceDate,
+		LastModified: payload.LastModified,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	// get purchaseInvoice number
+	purchaseOrderInvoice, err := h.poInvoiceStore.GetPurchaseOrderInvoiceByNumber(payload.Number)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("purchase order invoice number %d doesn't exists", payload.Number))
+		return
+	}
+
+	for _, medicine := range payload.MedicineLists {
+		medData, err := h.medStore.GetMedicineByBarcode(medicine.MedicineBarcode)
+		if err != nil {
+			utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("medicine %s doesn't exists", medicine.MedicineName))
+			return
+		}
+
+		unit, err := h.unitStore.GetUnitByName(medicine.Unit)
+		if unit == nil {
+			err = h.unitStore.CreateUnit(medicine.Unit)
+			if err != nil {
+				utils.WriteError(w, http.StatusInternalServerError, err)
+				return
+			}
+
+			unit, err = h.unitStore.GetUnitByName(medicine.Unit)
+		}
+		if err != nil {
+			utils.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		err = h.poInvoiceStore.CreatePurchaseOrderItems(types.PurchaseOrderItem{
+			PurchaseOrderInvoiceID: purchaseOrderInvoice.ID,
+			MedicineID:             medData.ID,
+			OrderQty:               medicine.OrderQty,
+			ReceivedQty:            medicine.ReceivedQty,
+			UnitID:                 unit.ID,
+			Remarks:                medicine.Remarks,
+		})
+		if err != nil {
+			utils.WriteError(w, http.StatusInternalServerError,
+				fmt.Errorf("purchase order invoice %d, med %s: %v", payload.Number, medicine.MedicineName, err))
+			return
+		}
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, fmt.Sprintf("purchase order invoice %d successfully created by %s", payload.Number, cashier.Name))
+}
+
+// only view the purchase invoice list
+func (h *Handler) handleGetPurchaseOrderInvoices(w http.ResponseWriter, r *http.Request) {
+	// get JSON Payload
+	var payload types.ViewPurchaseOrderInvoicePayload
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	// validate token
+	_, err := h.cashierStore.ValidateCashierToken(w, r, false)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("cashier token invalid"))
+		return
+	}
+
+	purchaseOrderInvoices, err := h.poInvoiceStore.GetPurchaseOrderInvoices(payload.StartDate, payload.EndDate)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, purchaseOrderInvoices)
+}
+
+// only view the purchase invoice list
+func (h *Handler) handleGetPurchaseOrderItems(w http.ResponseWriter, r *http.Request) {
+	// get JSON Payload
+	var payload types.ViewPurchaseOrderItemsPayload
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	// validate token
+	_, err := h.cashierStore.ValidateCashierToken(w, r, false)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("cashier token invalid"))
+		return
+	}
+
+	// get purchase order invoice data
+	purchaseOrderInvoice, err := h.poInvoiceStore.GetPurchaseOrderInvoiceByID(payload.PurchaseOrderInvoiceID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("purchase order invoice id %d doesn't exists", payload.PurchaseOrderInvoiceID))
+		return
+	}
+
+	// get medicine items of the purchase invoice
+	purchaseOrderItems, err := h.poInvoiceStore.GetPurchaseOrderItems(payload.PurchaseOrderInvoiceID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	// get company profile
+	company, err := h.companyProfileStore.GetCompanyProfileByID(purchaseOrderInvoice.CompanyID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("company id %d doesn't exists", purchaseOrderInvoice.CompanyID))
+		return
+	}
+
+	// get supplier data
+	supplier, err := h.supplierStore.GetSupplierByID(purchaseOrderInvoice.SupplierID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("supplier id %d doesn't exists", purchaseOrderInvoice.SupplierID))
+		return
+	}
+
+	// get cashier data, the one who inputs the purchase invoice
+	inputter, err := h.cashierStore.GetCashierByID(purchaseOrderInvoice.CashierID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("cashier id %d doesn't exists", purchaseOrderInvoice.CashierID))
+		return
+	}
+
+	returnPayload := types.PurchaseOrderInvoiceReturnJSONPayload{
+		PurchaseOrderInvoiceID:          purchaseOrderInvoice.ID,
+		PurchaseOrderInvoiceNumber:      purchaseOrderInvoice.Number,
+		PurchaseOrderInvoiceTotalItems:    purchaseOrderInvoice.TotalItems,
+		PurchaseOrderInvoiceInvoiceDate: purchaseOrderInvoice.InvoiceDate,
+		PurchaseOrderInvoiceLastModified: purchaseOrderInvoice.LastModified,
+
+		CompanyID:               company.ID,
+		CompanyName:             company.Name,
+		CompanyAddress:          company.Address,
+		CompanyBusinessNumber:   company.BusinessNumber,
+		Pharmacist:              company.Pharmacist,
+		PharmacistLicenseNumber: company.PharmacistLicenseNumber,
+
+		SupplierID:                  supplier.ID,
+		SupplierName:                supplier.Name,
+		SupplierAddress:             supplier.Address,
+		SupplierPhoneNumber:         supplier.CompanyPhoneNumber,
+		SupplierContactPersonName:   supplier.ContactPersonName,
+		SupplierContactPersonNumber: supplier.ContactPersonNumber,
+		SupplierTerms:               supplier.Terms,
+		SupplierVendorIsTaxable:     supplier.VendorIsTaxable,
+
+		CashierID:   inputter.ID,
+		CashierName: inputter.Name,
+
+		MedicineLists: purchaseOrderItems,
+	}
+
+	utils.WriteJSON(w, http.StatusOK, returnPayload)
+}
+
+func (h *Handler) handleDelete(w http.ResponseWriter, r *http.Request) {
+	// get JSON Payload
+	var payload types.DeletePurchaseOrderInvoice
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	// validate token
+	cashier, err := h.cashierStore.ValidateCashierToken(w, r, true)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("cashier token invalid or not admin: %v", err))
+		return
+	}
+
+	// check if the purchase invoice exists
+	purchaseOrderInvoice, err := h.poInvoiceStore.GetPurchaseOrderInvoiceByID(payload.ID)
+	if purchaseOrderInvoice == nil || err != nil {
+		utils.WriteError(w, http.StatusBadRequest,
+			fmt.Errorf("purchase invoice id %d doesn't exist", payload.ID))
+		return
+	}
+
+	err = h.poInvoiceStore.DeletePurchaseOrderItems(purchaseOrderInvoice.ID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = h.poInvoiceStore.DeletePurchaseOrderInvoice(purchaseOrderInvoice)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, fmt.Sprintf("purchase order invoice number %d deleted by %s", purchaseOrderInvoice.Number, cashier.Name))
+}
+
+func (h *Handler) handleModify(w http.ResponseWriter, r *http.Request) {
+	// get JSON Payload
+	var payload types.ModifyPurchaseOrderInvoicePayload
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	// validate token
+	cashier, err := h.cashierStore.ValidateCashierToken(w, r, false)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("cashier token invalid"))
+		return
+	}
+
+	// check if the purchase order invoice exists
+	_, err = h.poInvoiceStore.GetPurchaseOrderInvoiceByID(payload.PurchaseOrderInvoiceID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest,
+			fmt.Errorf("purchase order invoice with id %d doesn't exists", payload.PurchaseOrderInvoiceID))
+		return
+	}
+
+	err = h.poInvoiceStore.ModifyPurchaseOrderInvoice(payload.PurchaseOrderInvoiceID, types.PurchaseOrderInvoice{
+		Number:      payload.NewNumber,
+		CompanyID:   payload.NewCompanyID,
+		SupplierID:  payload.NewSupplierID,
+		CashierID:   cashier.ID,
+		TotalItems: payload.NewTotalItems,
+		InvoiceDate: payload.NewInvoiceDate,
+		LastModified: payload.NewLastModified,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = h.poInvoiceStore.DeletePurchaseOrderItems(payload.PurchaseOrderInvoiceID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	for _, medicine := range payload.NewMedicineLists {
+		medData, err := h.medStore.GetMedicineByBarcode(medicine.MedicineBarcode)
+		if err != nil {
+			utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("medicine %s doesn't exists", medicine.MedicineName))
+			return
+		}
+
+		unit, err := h.unitStore.GetUnitByName(medicine.Unit)
+		if unit == nil {
+			err = h.unitStore.CreateUnit(medicine.Unit)
+			if err != nil {
+				utils.WriteError(w, http.StatusInternalServerError, err)
+				return
+			}
+
+			unit, err = h.unitStore.GetUnitByName(medicine.Unit)
+		}
+		if err != nil {
+			utils.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		err = h.poInvoiceStore.CreatePurchaseOrderItems(types.PurchaseOrderItem{
+			PurchaseOrderInvoiceID: payload.PurchaseOrderInvoiceID,
+			MedicineID:             medData.ID,
+			OrderQty:               medicine.OrderQty,
+			ReceivedQty:            medicine.ReceivedQty,
+			UnitID:                 unit.ID,
+			Remarks:                medicine.Remarks,
+		})
+		if err != nil {
+			utils.WriteError(w, http.StatusInternalServerError,
+				fmt.Errorf("purchase order invoice %d, med %s: %v", payload.NewNumber, medicine.MedicineName, err))
+			return
+		}
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, fmt.Sprintf("purchase order invoice modified by %s", cashier.Name))
+}
---
+++ BACKEND/service/poinvoice/store.go
@@ -0,0 +1,240 @@
+package poinvoice
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/nicolaics/pos_pharmacy/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetPurchaseOrderInvoiceByNumber(number int) (*types.PurchaseOrderInvoice, error) {
+	rows, err := s.db.Query("SELECT * FROM purchase_order_invoice WHERE number = ?", number)
+	if err != nil {
+		return nil, err
+	}
+
+	purchaseOrderInvoice := new(types.PurchaseOrderInvoice)
+
+	for rows.Next() {
+		purchaseOrderInvoice, err = scanRowIntoPurchaseOrderInvoice(rows)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if purchaseOrderInvoice.ID == 0 {
+		return nil, fmt.Errorf("purchase order invoice not found")
+	}
+
+	return purchaseOrderInvoice, nil
+}
+
+func (s *Store) GetPurchaseOrderInvoiceByID(id int) (*types.PurchaseOrderInvoice, error) {
+	rows, err := s.db.Query("SELECT * FROM purchase_order_invoice WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	purchaseOrderInvoice := new(types.PurchaseOrderInvoice)
+
+	for rows.Next() {
+		purchaseOrderInvoice, err = scanRowIntoPurchaseOrderInvoice(rows)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if purchaseOrderInvoice.ID == 0 {
+		return nil, fmt.Errorf("purchase order invoice not found")
+	}
+
+	return purchaseOrderInvoice, nil
+}
+
+func (s *Store) CreatePurchaseOrderInvoice(poInvoice types.PurchaseOrderInvoice) error {
+	fields := "number, company_id, supplier_id, cashier_id, total_items, "
+	fields += "invoice_date, last_modified"
+	values := "?"
+
+	for i := 0; i < 6; i++ {
+		values += ", ?"
+	}
+
+	_, err := s.db.Exec(fmt.Sprintf("INSERT INTO purchase_order_invoice (%s) VALUES (%s)", fields, values),
+						poInvoice.Number, poInvoice.CompanyID, poInvoice.SupplierID,
+						poInvoice.CashierID, poInvoice.TotalItems, poInvoice.InvoiceDate,
+						poInvoice.LastModified)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) CreatePurchaseOrderItems(purchaseMedItem types.PurchaseOrderItem) error {
+	fields := "purchase_order_invoice_id, medicine_id, order_qty, received_qty, unit_id, remarks"
+	values := "?"
+
+	for i := 0; i < 5; i++ {
+		values += ", ?"
+	}
+
+	_, err := s.db.Exec(fmt.Sprintf("INSERT INTO purchase_order_items (%s) VALUES (%s)", fields, values),
+						purchaseMedItem.PurchaseOrderInvoiceID, purchaseMedItem.MedicineID, purchaseMedItem.OrderQty,
+						purchaseMedItem.ReceivedQty, purchaseMedItem.UnitID, purchaseMedItem.Remarks)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+func (s *Store) GetPurchaseOrderInvoices(startDate time.Time, endDate time.Time) ([]types.PurchaseOrderInvoice, error) {
+	query := fmt.Sprintf("SELECT * FROM purchase_order_invoice WHERE invoice_date BETWEEN DATE('%s') AND DATE('%s') ORDER BY invoice_date DESC",
+				startDate, endDate)
+
+	rows, err := s.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	purchaseOrderInvoices := make([]types.PurchaseOrderInvoice, 0)
+
+	for rows.Next() {
+		purchaseOrderInvoice, err := scanRowIntoPurchaseOrderInvoice(rows)
+
+		if err != nil {
+			return nil, err
+		}
+
+		purchaseOrderInvoices = append(purchaseOrderInvoices, *purchaseOrderInvoice)
+	}
+
+	return purchaseOrderInvoices, nil
+}
+
+
+func (s *Store) GetPurchaseOrderItems(purchaseOrderInvoiceId int) ([]types.PurchaseOrderItemsReturn, error) {
+	query := "SELECT "
+
+	query += "poi.id, medicine.barcode, medicine.name, poi.order_qty, poi.received_qty, "
+	query += "unit.unit, poi.remarks "
+
+	query += "FROM purchase_order_items as poi "
+	query += "JOIN purchase_order_invoice ON poi.purchase_order_invoice_id = purchase_order_invoice.id "
+	query += "JOIN medicine ON pmi.medicine_id = medicine.id "
+	query += "JOIN unit ON pmi.unit_id = unit.id "
+	query += "WHERE purchase_order_invoice.id = ? "
+
+	rows, err := s.db.Query(query, purchaseOrderInvoiceId)
+	if err != nil {
+		return nil, err
+	}
+
+	purchaseOrderItems := make([]types.PurchaseOrderItemsReturn, 0)
+
+	for rows.Next() {
+		purchaseOrderItem, err := scanRowIntoPurchaseOrderItems(rows)
+
+		if err != nil {
+			return nil, err
+		}
+
+		purchaseOrderItems = append(purchaseOrderItems, *purchaseOrderItem)
+	}
+
+	return purchaseOrderItems, nil
+}
+
+func (s *Store) DeletePurchaseOrderInvoice(purchaseOrderInvoice *types.PurchaseOrderInvoice) error {
+	_, err := s.db.Exec("DELETE FROM purchase_order_invoice WHERE id = ?", purchaseOrderInvoice.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) DeletePurchaseOrderItems(purchaseOrderInvoiceId int) error {
+	_, err := s.db.Exec("DELETE FROM purchase_order_items WHERE purchase_order_invoice_id = ? ", purchaseOrderInvoiceId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) ModifyPurchaseOrderInvoice(id int, purchaseOrderInvoice types.PurchaseOrderInvoice) error {
+	fields := "number = ?, company_id = ?, supplier_id = ?, cashier_id = ?, total_items = ?, "
+	fields += "invoice_date = ?, last_modified = ?"
+
+	query := fmt.Sprintf("UPDATE purchase_order_invoice SET %s WHERE id = ?", fields)
+
+	_, err := s.db.Exec(query,
+						purchaseOrderInvoice.Number, purchaseOrderInvoice.CompanyID, purchaseOrderInvoice.SupplierID,
+						purchaseOrderInvoice.CashierID, purchaseOrderInvoice.TotalItems, purchaseOrderInvoice.InvoiceDate,
+						purchaseOrderInvoice.LastModified, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func scanRowIntoPurchaseOrderInvoice(rows *sql.Rows) (*types.PurchaseOrderInvoice, error) {
+	purchaseOrderInvoice := new(types.PurchaseOrderInvoice)
+
+	err := rows.Scan(
+		&purchaseOrderInvoice.ID,
+		&purchaseOrderInvoice.Number,
+		&purchaseOrderInvoice.CompanyID,
+		&purchaseOrderInvoice.SupplierID,
+		&purchaseOrderInvoice.CashierID,
+		&purchaseOrderInvoice.TotalItems,
+		&purchaseOrderInvoice.InvoiceDate,
+		&purchaseOrderInvoice.LastModified,
+		&purchaseOrderInvoice.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	purchaseOrderInvoice.InvoiceDate = purchaseOrderInvoice.InvoiceDate.Local()
+	purchaseOrderInvoice.CreatedAt = purchaseOrderInvoice.CreatedAt.Local()
+	purchaseOrderInvoice.LastModified = purchaseOrderInvoice.LastModified.Local()
+
+	return purchaseOrderInvoice, nil
+}
+
+func scanRowIntoPurchaseOrderItems(rows *sql.Rows) (*types.PurchaseOrderItemsReturn, error) {
+	purchaseOrderItem := new(types.PurchaseOrderItemsReturn)
+
+	err := rows.Scan(
+		&purchaseOrderItem.ID,
+		&purchaseOrderItem.MedicineBarcode,
+		&purchaseOrderItem.MedicineName,
+		&purchaseOrderItem.OrderQty,
+		&purchaseOrderItem.ReceivedQty,
+		&purchaseOrderItem.Unit,
+		&purchaseOrderItem.Remarks,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return purchaseOrderItem, nil
+}
