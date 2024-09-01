1512b6a71b0e1f62ffb451dc854da805ab70a65d
nicolaics <nicolai.christian.s@gmail.com>
2024-09-01 21:53:46 +0900

    remove types to self folder

--- BACKEND/types/types.go
+++ BACKEND/types/types.go
@@ -4,18 +4,9 @@ import (
 	"time"
 )
 
-type NewInvoice struct {
-	Number            int       `json:"number" validate:"required"`
-	CashierName       string    `json:"cashierName" validate:"required"`
-	CustomerName      string    `json:"customerName" validate:"required"`
-	Subtotal          float64   `json:"subtotal" validate:"required"`
-	Discount          float64   `json:"discount"`
-	TotalPrice        float64   `json:"totalPrice" validate:"required"`
-	PaymentMethodName string    `json:"paymentMethodName" validate:"required"`
-	PaidAmount        float64   `json:"paidAmount" validate:"required"`
-	ChangeAmount      float64   `json:"changeAmount" validate:"required"`
-	Description       string    `json:"description"`
-	InvoiceDate       time.Time `json:"invoiceDate" validate:"required"`
+type PaymentMethodStore interface {
+	GetPaymentMethodByName(paymentMethodName string) (*PaymentMethod, error)
+	CreatePaymentMethod(paymentMethodName string) error
 }
 
 type PaymentMethod struct {
@@ -32,40 +23,6 @@ type Unit struct {
 	ID        int       `json:"id"`
 	Unit      string    `json:"unit"`
 	CreatedAt time.Time `json:"createdAt"`
-}
-
-type InvoiceStore interface {
-	GetInvoiceByID(id int) (*Invoice, error)
-	GetInvoicesByDate(date time.Time) ([]*Invoice, error)
-	CreateInvoice(Invoice) error
-}
-
-type Invoice struct {
-	ID                int       `json:"id"`
-	Number            int       `json:"number"`
-	CashierName       string    `json:"cashierName"`
-	CustomerName      string    `json:"customerName"`
-	Subtotal          float64   `json:"subtotal"`
-	Discount          float64   `json:"discount"`
-	TotalPrice        float64   `json:"totalPrice"`
-	PaidAmount        float64   `json:"paidAmount"`
-	ChangeAmount      float64   `json:"changeAmount"`
-	PaymentMethodName string    `json:"paymentMethodName"`
-	Description       string    `json:"description"`
-	InvoiceDate       time.Time `json:"invoiceDate"`
-	CreatedAt         time.Time `json:"createdAt"`
-}
-
-type MedicineItems struct {
-	ID              int       `json:"id"`
-	InvoiceID       int       `json:"invoiceId"`
-	MedicineBarcode string    `json:"medicineBarcode"`
-	Qty             float64   `json:"qty"`
-	UnitID          string    `json:"unitId"`
-	Price           float64   `json:"price"`
-	Discount        float64   `json:"discount"`
-	Subtotal        float64   `json:"subtotal"`
-	CreatedAt       time.Time `json:"createdAt"`
 }
 
 type TokenDetails struct {
