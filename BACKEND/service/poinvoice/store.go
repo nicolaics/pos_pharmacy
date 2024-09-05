b9838505c1564b0002f319df622d31019d40aec1
nicolaics <nicolai.christian.s@gmail.com>
2024-09-05 21:41:57 +0900

    add function to find invoice id only

--- BACKEND/service/poinvoice/store.go
+++ BACKEND/service/poinvoice/store.go
@@ -62,9 +62,11 @@ func (s *Store) GetPurchaseOrderInvoiceByID(id int) (*types.PurchaseOrderInvoice
 	return purchaseOrderInvoice, nil
 }
 
-func (s *Store) GetPurchaseOrderInvoiceByAll(number int, companyId int, supplierId int, userId int, totalItems int, invoiceDate time.Time) (int, error) {
-	query := `SELECT id FROM purchase_order_invoice WHERE number = ? AND company_id ? AND 
-	supplier_id = ? AND userId = ? AND total_items = ? AND invoice_date ? AND deleted_at IS NULL`
+func (s *Store) GetPurchaseOrderInvoiceID(number int, companyId int, supplierId int, userId int, totalItems int, invoiceDate time.Time) (int, error) {
+	query := `SELECT id FROM purchase_order_invoice 
+				WHERE number = ? AND company_id ? 
+				AND supplier_id = ? AND userId = ? AND total_items = ? 
+				AND invoice_date ? AND deleted_at IS NULL`
 
 	rows, err := s.db.Query(query, number, companyId, supplierId, userId, totalItems, invoiceDate)
 	if err != nil {
