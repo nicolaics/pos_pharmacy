4cf2d337ed78f195f7f3b57fe95925f1d44b180c
nicolaics <nicolai.christian.s@gmail.com>
2024-09-07 01:05:30 +0900

    init admin program

---
+++ BACKEND/cmd/init/InitAdmin.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/nicolaics/pos_pharmacy/config"
+	"github.com/nicolaics/pos_pharmacy/db"
+	"github.com/nicolaics/pos_pharmacy/service/auth"
+	"github.com/nicolaics/pos_pharmacy/utils"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	row := db.QueryRow("SELECT COUNT(*) FROM user")
+	if row.Err() != nil {
+		log.Fatal(row.Err())
+	}
+
+	var cnt int
+	err = row.Scan(&cnt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cnt != 0 {
+		log.Fatal("initial admin already exist!")
+	}
+	
+	password := utils.GenerateRandomCodeAlphanumeric(12)
+
+	// create new admin
+	hashedPassword, err := auth.HashPassword(password)
+	if err != nil {
+		log.Fatalf("failed to hashed password: %v", err)
+		return
+	}
+
+	args := os.Args
+
+	query := `INSERT INTO user (
+		name, password, admin, phone_number
+		) VALUES (?, ?, ?, ?)`
+	
+	_, err = db.Exec(query, args[1], hashedPassword, true, "000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fh, err := os.OpenFile("admin.txt", os.O_CREATE | os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fh.Close()
+
+	msg := fmt.Sprintf("Username: %s\nPassword: %s", args[1], password)
+	_, err = fh.WriteString(msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
