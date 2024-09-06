f858c0c9f5c4ce304a2717014fa8849df7867372
nicolaics <nicolai.christian.s@gmail.com>
2024-09-07 01:03:44 +0900

    change field name

--- BACKEND/types/types.go
+++ BACKEND/types/types.go
@@ -12,7 +12,7 @@ type PaymentMethodStore interface {
 
 type PaymentMethod struct {
 	ID        int       `json:"id"`
-	Name    string    `json:"name"`
+	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"createdAt"`
 }
 
