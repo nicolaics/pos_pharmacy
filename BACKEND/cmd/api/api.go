b63ed8310df7eb26a35436f5594f790d0bee2c9c
nicolaics <nicolai.christian.s@gmail.com>
2024-09-09 03:49:52 +0900

    add cors middleware

--- BACKEND/cmd/api/api.go
+++ BACKEND/cmd/api/api.go
@@ -108,67 +108,10 @@ func (s *APIServer) Run() error {
 
 	logMiddleware := logger.NewLogMiddleware(loggerVar)
 	router.Use(logMiddleware.Func())
-	// subrouter.Use(auth.SessionMiddleware())
+
+	router.Use(auth.CorsMiddleware())
 	subrouter.Use(auth.AuthMiddleware())
+	
 
 	return http.ListenAndServe(s.addr, router)
 }
-
-// TODO: VERIFY FOR SESSION MIDDLEWARE, so everytime they access page, check
-// var (
-// 	store = sessions.NewCookieStore([]byte("test"))
-// )
-
-// func sessionMiddleware(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		// todo admin 로그인에 대해서 static 폴더에 대한 파일은 예외처리
-
-// 		// 세션 검사
-// 		session, _ := store.Get(r, "session-name")
-// 		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-// 			http.Error(w, "Forbidden", http.StatusForbidden)
-// 			return
-// 		}
-
-// 		// 다음 핸들러 또는 미들웨어로 요청 전달
-// 		next.ServeHTTP(w, r)
-// 	})
-// }
-
-// func checkSession(r *http.Request) bool {
-// 	session, err := store.Get(r, "session-name")
-// 	if err != nil {
-// 		// 세션 가져오기 실패
-// 		return false
-// 	}
-
-// 	// 세션에 'authenticated' 값이 있는지 확인
-// 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-// 		return false
-// 	}
-// 	return true
-// }
-
-// TODO: login successfully, set the authentification to true
-// TODO: check as well from the JWT token authorized
-// if adminID == "jelly" && password == "walk" {
-// 	session, _ := store.Get(r, "session-name")
-// 	session.Values["authenticated"] = true
-// 	session.Save(r, w)
-
-// 	http.Redirect(w, r, "/admin/", http.StatusFound)
-// 	return
-// }
-
-// TODO: if failed redirect
-// http.Redirect(w, r, "/admin/login", http.StatusFound)
-
-// TODO: after logout, set the authentification to false
-// func handleLogout(w http.ResponseWriter, r *http.Request) {
-
-// 	session, _ := store.Get(r, "session-name")
-// 	session.Values["authenticated"] = false
-// 	session.Save(r, w)
-
-// 	http.Redirect(w, r, "/admin-login", http.StatusFound)
-// }
