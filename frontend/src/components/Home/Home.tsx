2a217472b0f681c031a1fd903917b0dbb4c9c17e
nicolaics <nicolai.christian.s@gmail.com>
2024-09-09 03:47:51 +0900

    add auth middleware

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,4 +1,9 @@
-import React, { SyntheticEvent, useEffect, useLayoutEffect, useState } from "react";
+import React, {
+  SyntheticEvent,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
 import {
   FaLock,
   FaShoppingCart,
@@ -7,12 +12,13 @@ import {
   FaUser,
   FaClock,
 } from "react-icons/fa";
-
+import { FaUserDoctor } from "react-icons/fa6";
 import { IoIosLogOut } from "react-icons/io";
-import "./Home.css";
-import { Navigate, useNavigate } from "react-router-dom";
+import { MdSick } from "react-icons/md";
 
-const url = "http://localhost:19230/api/v1/user/verify";
+import "./Home.css";
+import { Navigate, NavigateFunction, useNavigate } from "react-router-dom";
+import { ApplyMiddleware, AuthMiddleware, RequestContext } from "../../Middleware";
 
 // TODO: think about back navigation
 // TODO: if the user goes back, reverify the token in the local storage
@@ -21,19 +27,53 @@ const LandingPage: React.FC = () => {
   const navigate = useNavigate();
 
   const logout = () => {
-    sessionStorage.removeItem("token");
-    navigate("/", { replace: true });
+    const token = sessionStorage.getItem("token");
+    const logoutURL = "http://localhost:19230/api/v1/user/logout";
+
+    const requestContext: RequestContext = {
+      url: logoutURL,
+      options: {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + token,
+        },
+      },
+    };
+
+    console.log("token ", token);
+
+    ApplyMiddleware(AuthMiddleware, requestContext).then((response) =>
+      response
+        .json()
+        .then((data) => {
+          if (!response.ok) {
+            alert("token invalid");
+            throw response;
+          }
+
+          console.log(data);
+          sessionStorage.removeItem("token");
+          navigate("/", { replace: true });
+        })
+        .catch((error) => {
+          console.error("Error logging out:", error);
+        })
+    );
   };
 
-  console.log("before: ", sessionStorage.getItem("token"));
+  const user = () => {
+    navigate("/user")
+  };
+
 
   return (
     <div className="landing-page">
       <h1>Welcome!</h1>
       <div className="grid-container">
-        <div className="grid-item">
+        <div className="grid-item" onClick={user}>
           <FaLock size={50} />
-          <h2>Admin</h2>
+          <h2>User</h2>
         </div>
         <div className="grid-item">
           <FaUser size={50} />
@@ -42,6 +82,14 @@ const LandingPage: React.FC = () => {
         <div className="grid-item">
           <FaShoppingCart size={50} />
           <h2>Supplier</h2>
+        </div>
+        <div className="grid-item">
+          <MdSick size={50} />
+          <h2>Patient</h2>
+        </div>
+        <div className="grid-item">
+          <FaUserDoctor size={50} />
+          <h2>Doctor</h2>
         </div>
         <div className="grid-item">
           <FaPills size={50} />
@@ -55,7 +103,7 @@ const LandingPage: React.FC = () => {
           <FaClock size={50} />
           <h2>Purchasing</h2>
         </div>
-        <div className="logout-item" onClick={logout}>
+        <div className="grid-item" onClick={logout}>
           <IoIosLogOut size={50} />
           <h2>Logout</h2>
         </div>
--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,86 +1,109 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { useNavigate } from "react-router-dom";
-import LandingPage from "../Home/Home";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import {
+  ApplyMiddleware,
+  AuthMiddleware,
+  RequestContext,
+} from "../../Middleware";
 
 // so we will define our constants here
-const url = 'http://localhost:19230/api/v1/cashier/login';
+const url = "http://localhost:19230/api/v1/user/login";
 
-const username = 'admin';
-const password = 'admin1234!';
+// const username = 'admin1';
+// const password = 'dnP9K5RMjV1l';
 
-// now we configure the fetch request to the url endpoint.
-// we should probably put it inside a separate function since
-// you're using a browser, you probably will bind this request
-// to a click event or something.
-async function login() {
-  const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: username,
-            password: password,
-        }),
-    });
-    // An important thing to note is that an error response will not throw
-    // an error so if the result is not okay we should throw the error
-    if (!response.ok) {
-        throw response;
-    }
-    return await response.json();
+function login(username: string, password: string, navigate: NavigateFunction) {
+  const requestContext: RequestContext = {
+    url: url,
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: username,
+        password: password,
+      }),
+    },
+  };
+
+  ApplyMiddleware(AuthMiddleware, requestContext).then((response) =>
+    response
+      .json()
+      .then((data) => {
+        if (!response.ok) {
+          alert("wrong credentials");
+          throw response;
+        }
+
+        console.log(data);
+        sessionStorage.setItem("token", data["token"]);
+        navigate("/home");
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      })
+  );
 }
 
 const LoginPage: React.FC = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleUsernameChange = (event: any) => {
-        setUsername(event.target.value);
-    };
+  const handleUsernameChange = (event: any) => {
+    setUsername(event.target.value);
+  };
 
-    const handlePasswordChange = (event: any) => {
-        setPassword(event.target.value);
-    };
+  const handlePasswordChange = (event: any) => {
+    setPassword(event.target.value);
+  };
 
-    // TODO: fetch from API
-    const handleSubmit = (event: any) => {  
-        event.preventDefault();
+  // TODO: fetch from API
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
 
-        if (username === "" || password === "") {
-            alert(`username and password cannot be empty`);
-            return;
-        }
+    if (username === "" || password === "") {
+      alert(`username and password cannot be empty`);
+      return;
+    }
 
-        // console.log(login());
-        // setUsername("");
-        // setPassword("");
+    login(username, password, navigate);
 
-        sessionStorage.setItem("token", "asc");
-        navigate("/Home");
-    };
+    setUsername("");
+    setPassword("");
+  };
 
-    return (
-        <div className="login-grid">
-            <h1>Login</h1>
-            <span className="login-form">
-                <form onSubmit={handleSubmit}>
-                    <label id="username">Username: </label>
-                    <input type="text" id="username" value={username} onChange={handleUsernameChange}/>
-                    <br />
-                        
-                    <label id="password">Password: </label>
-                    <input type="password" id="password" value={password} onChange={handlePasswordChange} />
-                    <br />
-                    
-                    <input type="submit" value={"Login"} />
-                </form>
-            </span>
-        </div>
-    );
+  return (
+    <div className="login-grid">
+      <h1>Login</h1>
+      <span className="login-form">
+        <form onSubmit={handleSubmit}>
+          <label id="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <br />
+
+          <label id="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <br />
+
+          <input type="submit" value={"Login"} />
+        </form>
+      </span>
+    </div>
+  );
 };
 
 export default LoginPage;
