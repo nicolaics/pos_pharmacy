63cab8be33af96ccaa398fff51c627169b28e9ec
nicolaics <nicolai.christian.s@gmail.com>
2024-09-09 03:48:21 +0900

    add user page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import LandingPage from "./components/Home/Home";
 import LoginPage from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import UserPage from "./components/User/User";
 
 const App: React.FC = () => {
   return (
     <div className="App">      
       <Routes>
         <Route path="/" element={<LoginPage/>} />
-        <Route path="/Home" element={<LandingPage/>} />
+        <Route path="/home" element={<LandingPage/>} />
+        <Route path="/user" element={<UserPage/>} />
       </Routes>
     </div>
   );
