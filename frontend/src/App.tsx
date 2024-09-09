efc4fbd4c708298a975e77c75f9640c05ca1e8fc
nicolaics <nicolai.christian.s@gmail.com>
2024-09-09 22:02:04 +0900

    add AddUser page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,36 @@ import LandingPage from "./components/Home/Home";
 import LoginPage from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import UserPage from "./components/User/User";
+import ProtectedRoute from "./ProtectedRoute";
+import AddUserPage from "./components/User/AddUser";
 
 const App: React.FC = () => {
   return (
-    <div className="App">      
+    <div className="App">
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
-        <Route path="/home" element={<LandingPage/>} />
-        <Route path="/user" element={<UserPage/>} />
+        <Route path="/" element={<LoginPage />} />
+
+        <Route
+          path="/home"
+          element={<ProtectedRoute children={<LandingPage />} admin={false} />}
+        />
+
+        <Route
+          path="/user"
+          element={<ProtectedRoute children={<UserPage />} admin={false} />}
+        />
+
+        <Route
+          path="/user/add"
+          element={<ProtectedRoute children={<AddUserPage />} admin={true} />}
+        />
+
+        { /* FOR TESTING */ }
+        {/* <Route
+          path="/user/add"
+          element={<AddUserPage />}
+        /> */}
+
       </Routes>
     </div>
   );
