af90a098637f67647f1943531b80e771e1ae4872
nicolaics <nicolai.christian.s@gmail.com>
2024-09-10 21:51:08 +0900

    add supplier page with styling

---
+++ frontend/src/components/Supplier/LandingPage/Supplier.tsx
@@ -0,0 +1,217 @@
+import React, { useState } from "react";
+
+import "./Supplier.css";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import FormatDateTime from "../../../DateTimeFormatter";
+import { FaHome, FaSearch } from "react-icons/fa";
+import { BsBuildingFillAdd } from "react-icons/bs";
+
+function fillTable(
+  data: any,
+  tableBody: Element | null,
+  navigate: NavigateFunction
+) {
+  if (!tableBody) return;
+
+  // Loop through each supplier and create a new row in the table
+  const row = document.createElement("tr");
+  row.className = "view-supplier-data-row";
+
+  // Create and append cells for each column
+  const idCell = document.createElement("td");
+  idCell.textContent = data["id"].toString();
+  row.appendChild(idCell);
+
+  const nameCell = document.createElement("td");
+  nameCell.textContent = data["name"];
+  row.appendChild(nameCell);
+
+  const addressCell = document.createElement("td");
+  addressCell.textContent = data["address"];
+  row.appendChild(addressCell);
+
+  const companyPhoneNumberCell = document.createElement("td");
+  companyPhoneNumberCell.textContent = data["companyPhoneNumber"];
+  row.appendChild(companyPhoneNumberCell);
+
+  const contactPersonNameCell = document.createElement("td");
+  contactPersonNameCell.textContent = data["contactPersonName"];
+  row.appendChild(contactPersonNameCell);
+
+  const contactPersonNumberCell = document.createElement("td");
+  contactPersonNumberCell.textContent = data["contactPersonNumber"];
+  row.appendChild(contactPersonNumberCell);
+
+  const termsCell = document.createElement("td");
+  termsCell.textContent = data["terms"];
+  row.appendChild(termsCell);
+
+  const vendorIsTaxableCell = document.createElement("td");
+  vendorIsTaxableCell.textContent = data["vendorIsTaxable"];
+  row.appendChild(vendorIsTaxableCell);
+
+  const lastModified = new Date(data["lastModified"]);
+  const lastModifiedCell = document.createElement("td");
+  lastModifiedCell.textContent = FormatDateTime(lastModified);
+  row.appendChild(lastModifiedCell);
+
+  const lastModifiedByUserIdCell = document.createElement("td");
+  lastModifiedByUserIdCell.textContent = data["lastModifiedByUserId"];
+  row.appendChild(lastModifiedByUserIdCell);
+
+  const createdAt = new Date(data["createdAt"]);
+  const createdAtCell = document.createElement("td");
+  createdAtCell.textContent = FormatDateTime(createdAt);
+  row.appendChild(createdAtCell);
+
+  row.addEventListener("dblclick", () => {
+    navigate("/supplier/detail", {
+      state: {
+        id: data["id"],
+        name: data["name"],
+      },
+    });
+  });
+
+  // Append the row to the table body
+  tableBody.appendChild(row);
+}
+
+const ViewSupplierPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const testData = [
+    {
+      id: 1,
+      name: "John Doe 1",
+      address: "ASdlasij",
+      companyPhoneNumber: "010-000-10200",
+      contactPersonName: "John",
+      contactPersonNumber: "000-0000-0000",
+      terms: "Y",
+      vendorIsTaxable: true,
+      createdAt: "2024-08-01 12.10",
+      lastModified: "2024-08-10 12.10",
+      lastModifiedByUserId: 1
+    },
+    {
+      id: 2,
+      name: "John Doe 2",
+      address: "ASdlasij",
+      companyPhoneNumber: "010-000-10200",
+      contactPersonName: "John",
+      contactPersonNumber: "000-0000-0000",
+      terms: "Y",
+      vendorIsTaxable: false,
+      createdAt: "2024-08-01 12.10",
+      lastModified: "2024-08-10 12.10",
+      lastModifiedByUserId: 1
+    },
+  ];
+
+  const search = () => {
+    const token = sessionStorage.getItem("token");
+    const getAllSupplierURL = "http://localhost:19230/api/v1/supplier";
+
+    // TEST DATA
+    const tableBody = document.querySelector("#supplier-data-table tbody");
+    if (!tableBody) {
+      console.error("table body not found");
+      return;
+    }
+    tableBody.innerHTML = "";
+    for (let i = 0; i < testData.length; i++) {
+      fillTable(testData[i], tableBody, navigate);
+    }
+
+    // fetch(getAllSupplierURL, {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: "Bearer " + token,
+    //   },
+    // })
+    //   .then((response) =>
+    //     response.json().then((data) => {
+    //       if (!response.ok) {
+    //         throw new Error("Invalid credentials or network issue");
+    //       }
+
+    //       const tableBody = document.querySelector("#dataTable tbody");
+    //       if (!tableBody) {
+    //         console.error("table body not found");
+    //         return;
+    //       }
+
+    //       tableBody.innerHTML = "";
+
+    //       for (let i = 0; i < data.length; i++) {
+    //         fillTable(data[i], tableBody, navigate);
+    //       }
+    //     })
+    //   )
+    //   .catch((error) => {
+    //     console.error("Error loading supplier data:", error);
+    //     alert("Error loading supplier data");
+    //   });
+  };
+
+  const returnToHome = () => {
+    navigate("/home");
+  };
+
+  const register = () => {
+    navigate("/supplier/detail");
+  };
+
+  return (
+    <div className="view-supplier-page">
+      <h1>Supplier</h1>
+
+      <div className="supplier-search-container">
+        <input
+          type="text"
+          className="supplier-search-input"
+          placeholder="Search"
+        />
+        <button onClick={search} className="supplier-search-button">
+          <FaSearch size={30} id="supplier-search-icon" />
+          Search
+        </button>
+
+        <button onClick={register} className="supplier-add-button">
+          <BsBuildingFillAdd size={30} id="supplier-add-icon" />
+          Add
+        </button>
+      </div>
+
+      <div className="supplier-table-container">
+        <table id="supplier-data-table" border={1}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Company Phone Number</th>
+              <th>Contact Person Name</th>
+              <th>Contact Person Number</th>
+              <th>Terms</th>
+              <th>Vendor is Taxable</th>
+              <th>Last Modified</th>
+              <th>Last Modified By ID</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+
+      <button onClick={returnToHome} className="view-supplier-home-button">
+        <FaHome size={30} id="view-supplier-home-icon" />
+        Back to Home
+      </button>
+    </div>
+  );
+};
+
+export default ViewSupplierPage;
---
+++ frontend/src/components/Supplier/Modify/Modify.tsx
@@ -0,0 +1,331 @@
+import React, { useEffect, useState } from "react";
+
+import "./Modify.css";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const ModifySupplierPage: React.FC = () => {
+  const navigate = useNavigate();
+  const state = useLocation().state;
+
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [companyPhoneNumber, setCompanyPhoneNumber] = useState("");
+  const [contactPersonName, setContactPersonName] = useState("");
+  const [contactPersonNumber, setContactPersonNumber] = useState("");
+  const [terms, setTerms] = useState("");
+  const [vendorIsTaxable, setVendorIsTaxable] = useState(true);
+
+  const [okBtnLabel, setOkBtnLabel] = useState("Modify");
+
+  var heading = "";
+  if (state) {
+    heading = "Modify";
+  } else {
+    heading = "Add";
+  }
+
+  /*
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    if (state) {
+      setOkBtnLabel("Modify");
+
+      // TODO: add query for searching suppliers
+      const supplierURL = "http://localhost:19230/api/v1/supplier?id="; // Set the URL or handle this logic
+      fetch(supplierURL, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then((response) =>
+          response.json().then((data) => {
+            if (!response.ok) {
+              throw new Error("Unable to modify supplier data");
+            }
+
+            console.log(data["data"]);
+
+            setId(data["data"].id);
+            setName(data["data"].name);
+          })
+        )
+        .catch((error) => {
+          console.error("Error load selected supplier:", error);
+          alert("Error load selected supplier");
+        });
+    } else {
+      setOkBtnLabel("Add");
+    }
+  }, [state]); // Dependency array ensures this effect only runs when reqType changes
+  */
+
+  const handleNameChange = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const handleAddressChange = (event: any) => {
+    setAddress(event.target.value);
+  }
+
+  const handleCompanyPhoneNumberChange = (event: any) => {
+    setCompanyPhoneNumber(event.target.value);
+  }
+
+  const handleContactPersonNameChange = (event: any) => {
+    setContactPersonName(event.target.value);
+  }
+
+  const handleContactPersonNumberChange = (event: any) => {
+    setContactPersonNumber(event.target.value);
+  }
+
+  const handleTermsChange = (event: any) => {
+    setTerms(event.target.value);
+  }
+
+  const handleVendorIsTaxableChange = (event: any) => {
+    if (event.target.value == "yes") {
+      setVendorIsTaxable(true);
+    }
+    else {
+      setVendorIsTaxable(false);
+    }
+  }
+
+  const handleSendRequest = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault(); // Prevent form submission
+
+    // Handle form submission logic here
+    const token = sessionStorage.getItem("token");
+
+    if (state) {
+      const url = "http://localhost:19230/api/v1/supplier";
+
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+        body: JSON.stringify({
+          name: name,
+        }),
+      })
+        .then((response) =>
+          response.json().then((data) => {
+            if (!response.ok) {
+              throw new Error("Unable to modify supplier data");
+            }
+
+            console.log(data);
+          })
+        )
+        .catch((error) => {
+          console.error("Error modify supplier:", error);
+          alert("Error modify supplier");
+        });
+    } else {
+      const url = "http://localhost:19230/api/v1/supplier";
+
+      fetch(url, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+        body: JSON.stringify({
+          id: Number(id),
+          newData: {
+            name: name,
+          },
+        }),
+      })
+        .then((response) =>
+          response.json().then((data) => {
+            if (!response.ok) {
+              throw new Error("Invalid c redentials or network issue");
+            }
+
+            console.log(data);
+          })
+        )
+        .catch((error) => {
+          console.error("Error adding new supplier:", error);
+          alert("Error adding new supplier");
+        });
+    }
+
+    // Reset the state
+    navigate("/supplier");
+  };
+
+  const handleCancel = (e: any) => {
+    navigate("/supplier");
+  };
+
+  const handleDelete = (e: any) => {
+    e.preventDefault();
+
+    const token = sessionStorage.getItem("token");
+    const url = "http://localhost:19230/api/v1/supplier";
+
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        id: Number(id),
+        name: name,
+      }),
+    })
+      .then((response) =>
+        response.json().then((data) => {
+          if (!response.ok) {
+            throw new Error("Unable to delete supplier data");
+          }
+
+          console.log(data);
+          navigate("/supplier");
+        })
+      )
+      .catch((error) => {
+        console.error("Error delete supplier:", error);
+        alert("Error delete supplier");
+      });
+  };
+
+  return (
+    <div className="modify-supplier-page">
+      <h1>{heading} Supplier</h1>
+
+      <div className="supplier-data-container">
+        
+        <div className="supplier-data-form-group">
+          <label htmlFor="id">ID:</label>
+          <input type="text" id="modify-supplier-id" value={id} readOnly />
+        </div>
+        
+        <div className="supplier-data-form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="modify-supplier-name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+
+        <div className="supplier-data-form-group">
+          <label htmlFor="address">Address:</label>
+          <textarea
+            id="modify-supplier-address"
+            value={address}
+            onChange={handleAddressChange}
+          />
+        </div>
+
+        <div className="supplier-data-form-group">
+          <label htmlFor="companyPhoneNumber">Company Phone Number:</label>
+          <input
+            type="text"
+            id="modify-supplier-companyPhoneNumber"
+            value={companyPhoneNumber}
+            onChange={handleCompanyPhoneNumberChange}
+          />
+        </div>
+
+        <div className="supplier-data-form-group">
+          <label htmlFor="contactPersonName">Contact Person Name:</label>
+          <input
+            type="text"
+            id="modify-supplier-contactPersonName"
+            value={contactPersonName}
+            onChange={handleContactPersonNameChange}
+          />
+        </div>
+
+        <div className="supplier-data-form-group">
+          <label htmlFor="contactPersonNumber">Contact Person Number:</label>
+          <input
+            type="text"
+            id="modify-supplier-contactPersonNumber"
+            value={contactPersonNumber}
+            onChange={handleContactPersonNumberChange}
+          />
+        </div>
+
+        <div className="supplier-data-form-group">
+          <label htmlFor="terms">Terms:</label>
+          <input
+            type="text"
+            id="modify-supplier-terms"
+            value={terms}
+            onChange={handleTermsChange}
+          />
+        </div>
+
+        <div className="supplier-data-form-group">
+            <label>Vendor is Taxable:</label>
+            <div className="supplier-data-radio-grp">
+              <input
+                type="radio"
+                id="modify-supplier-radio-yes"
+                checked={vendorIsTaxable === true}
+                name="vendorIsTaxable"
+                value={"yes"}
+                onChange={handleVendorIsTaxableChange}
+              />
+              <label htmlFor="radio-yes">Yes</label>
+              <input
+                type="radio"
+                checked={vendorIsTaxable === false}
+                id="modify-supplier-radio-no"
+                name="vendorIsTaxable"
+                value={"no"}
+                onChange={handleVendorIsTaxableChange}
+              />
+              <label htmlFor="radio-no">No</label>
+            </div>
+          </div>
+      </div>
+
+      <div className="modify-supplier-buttons">
+        <div className="modify-supplier-btns-grp">
+          <button
+            type="button"
+            className="modify-supplier-ok-btn"
+            onClick={handleSendRequest}
+          >
+            {okBtnLabel}
+          </button>
+
+          <button
+            type="button"
+            className="modify-supplier-cancel-btn"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+
+          <button
+            type="button"
+            className="modify-supplier-delete-btn"
+            onClick={handleDelete}
+          >
+            Delete Supplier
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModifySupplierPage;
