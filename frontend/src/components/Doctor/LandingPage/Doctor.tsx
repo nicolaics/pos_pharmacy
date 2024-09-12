6d95babede3c77e0257d7197bcb40daf5868401e
nicolaics <nicolai.christian.s@gmail.com>
2024-09-12 17:07:06 +0900

    add doctor page

---
+++ frontend/src/components/Doctor/LandingPage/Doctor.tsx
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+
+import "./Doctor.css";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import FormatDateTime from "../../../DateTimeFormatter";
+import { MdPersonSearch } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { BsPersonFillAdd } from "react-icons/bs";
+import { BACKEND_BASE_URL } from "../../../App";
+
+function fillTable(
+  data: any,
+  tableBody: Element | null,
+  navigate: NavigateFunction
+) {
+  if (!tableBody) return;
+
+  // Loop through each doctor and create a new row in the table
+  const row = document.createElement("tr");
+  row.className = "view-doctor-data-row";
+
+  // Create and append cells for each column
+  const idCell = document.createElement("td");
+  idCell.textContent = data["id"].toString();
+  row.appendChild(idCell);
+
+  const nameCell = document.createElement("td");
+  nameCell.textContent = data["name"];
+  row.appendChild(nameCell);
+
+  const createdAt = new Date(data["createdAt"]);
+  const createdAtCell = document.createElement("td");
+  createdAtCell.textContent = FormatDateTime(createdAt);
+  row.appendChild(createdAtCell);
+
+  row.addEventListener("dblclick", () => {
+    navigate("/doctor/detail", {
+      state: {
+        id: data["id"],
+        name: data["name"],
+      },
+    });
+  });
+
+  // Append the row to the table body
+  tableBody.appendChild(row);
+}
+
+const ViewDoctorPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const testData = [
+    {
+      id: 1,
+      name: "John Doe 1",
+      createdAt: "2024-08-01 12.10",
+    },
+    {
+      id: 2,
+      name: "John Doe 2",
+      createdAt: "2024-08-01 12.10",
+    },
+  ];
+
+  const search = () => {
+    const token = sessionStorage.getItem("token");
+    const getAllDoctorURL = `http://${BACKEND_BASE_URL}/doctor`;
+
+    // TEST DATA
+    const tableBody = document.querySelector("#doctor-data-table tbody");
+    if (!tableBody) {
+      console.error("table body not found");
+      return;
+    }
+    tableBody.innerHTML = "";
+    for (let i = 0; i < testData.length; i++) {
+      fillTable(testData[i], tableBody, navigate);
+    }
+
+    // fetch(getAllDoctorURL, {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: "Bearer " + token,
+    //   },
+    // })
+    //   .then((response) =>
+    //     response.json().then((data) => {
+    //       if (!response.ok) {
+    //         throw new Error("Invalid credentials or network issue");
+    //       }
+
+    //       const tableBody = document.querySelector("#dataTable tbody");
+    //       if (!tableBody) {
+    //         console.error("table body not found");
+    //         return;
+    //       }
+
+    //       tableBody.innerHTML = "";
+
+    //       for (let i = 0; i < data.length; i++) {
+    //         fillTable(data[i], tableBody, navigate);
+    //       }
+    //     })
+    //   )
+    //   .catch((error) => {
+    //     console.error("Error loading doctor data:", error);
+    //     alert("Error loading doctor data");
+    //   });
+  };
+
+  const returnToHome = () => {
+    navigate("/home");
+  };
+
+  const register = () => {
+    navigate("/doctor/detail");
+  };
+
+  return (
+    <div className="view-doctor-page">
+      <h1>Doctor</h1>
+      
+      <div className="doctor-container">
+        <input
+          type="text"
+          className="doctor-search-input"
+          placeholder="Search"
+        />
+        <button onClick={search} className="doctor-search-button">
+          <MdPersonSearch size={30} id="doctor-search-icon" />
+          Search
+        </button>
+
+        <button onClick={register} className="doctor-add-button">
+          <BsPersonFillAdd size={30} id="doctor-add-icon" />
+          Add
+        </button>
+      </div>
+
+      <div className="doctor-table">
+        <table id="doctor-data-table" border={1}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+
+      <button onClick={returnToHome} className="view-doctor-home-button">
+        <FaHome size={30} id="view-doctor-home-icon" />
+        Back to Home
+      </button>
+    </div>
+  );
+};
+
+export default ViewDoctorPage;
---
+++ frontend/src/components/Doctor/Modify/Modify.tsx
@@ -0,0 +1,228 @@
+import React, { useEffect, useState } from "react";
+
+import "./Modify.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import { BACKEND_BASE_URL } from "../../../App";
+
+const ModifyDoctorPage: React.FC = () => {
+  const navigate = useNavigate();
+  const state = useLocation().state;
+
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+
+  const [okBtnLabel, setOkBtnLabel] = useState("Modify");
+  const [showIdField, setShowIdField] = useState(false);
+
+  var heading = "";
+  if (state) {
+    heading = "Modify";
+  } else {
+    heading = "Add";
+  }
+
+  /*
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    if (state) {
+      setOkBtnLabel("Modify");
+      setShowIdField(true);
+
+      // TODO: add query for searching doctors
+      const doctorURL = `http://${BACKEND_BASE_URL}/doctor?all=all`; // Set the URL or handle this logic
+      fetch(doctorURL, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then((response) =>
+          response.json().then((data) => {
+            if (!response.ok) {
+              throw new Error("Unable to modify doctor data");
+            }
+
+            console.log(data["data"]);
+
+            setId(data["data"].id);
+            setName(data["data"].name);
+          })
+        )
+        .catch((error) => {
+          console.error("Error load selected doctor:", error);
+          alert("Error load selected doctor");
+        });
+    } else {
+      setOkBtnLabel("Add");
+      setShowIdField(false);
+    }
+  }, [state]); // Dependency array ensures this effect only runs when reqType changes
+  */
+
+
+  const handleNameChange = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const handleSendRequest = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault(); // Prevent form submission
+
+    // Handle form submission logic here
+    const token = sessionStorage.getItem("token");
+
+    if (state) {
+      const url = `http://${BACKEND_BASE_URL}/doctor`;
+
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+        body: JSON.stringify({
+          name: name,
+        }),
+      })
+        .then((response) =>
+          response.json().then((data) => {
+            if (!response.ok) {
+              throw new Error("Unable to modify doctor data");
+            }
+
+            console.log(data);
+          })
+        )
+        .catch((error) => {
+          console.error("Error modify doctor:", error);
+          alert("Error modify doctor");
+        });
+    } else {
+      const url = `http://${BACKEND_BASE_URL}/doctor`;
+
+      fetch(url, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+        body: JSON.stringify({
+          id: Number(id),
+          newData: {
+            name: name,
+          },
+        }),
+      })
+        .then((response) =>
+          response.json().then((data) => {
+            if (!response.ok) {
+              throw new Error("Invalid c redentials or network issue");
+            }
+
+            console.log(data);
+          })
+        )
+        .catch((error) => {
+          console.error("Error adding new doctor:", error);
+          alert("Error adding new doctor");
+        });
+    }
+
+    // Reset the state
+    navigate("/doctor");
+  };
+
+  const handleCancel = (e: any) => {
+    navigate("/doctor");
+  };
+
+  const handleDelete = (e: any) => {
+    e.preventDefault();
+
+    const token = sessionStorage.getItem("token");
+    const url = `http://${BACKEND_BASE_URL}/doctor`;
+
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        id: Number(id),
+        name: name,
+      }),
+    })
+      .then((response) =>
+        response.json().then((data) => {
+          if (!response.ok) {
+            throw new Error("Unable to delete doctor data");
+          }
+
+          console.log(data);
+          navigate("/doctor");
+        })
+      )
+      .catch((error) => {
+        console.error("Error delete doctor:", error);
+        alert("Error delete doctor");
+      });
+  };
+
+  return (
+    <div className="modify-doctor-page">
+      <h1>{heading} Doctor</h1>
+
+      <div className="doctor-data-container">
+        { showIdField && (
+        <div className="doctor-data-form-group">
+          <label htmlFor="id">ID:</label>
+          <input type="text" id="modify-doctor-id" value={id} readOnly />
+        </div>
+        )}
+        <div className="doctor-data-form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="modify-doctor-name"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+      </div>
+
+      <div className="modify-doctor-buttons">
+        <div className="modify-doctor-btns-grp">
+          <button
+            type="button"
+            className="modify-doctor-ok-btn"
+            onClick={handleSendRequest}
+          >
+            {okBtnLabel}
+          </button>
+
+          <button
+            type="button"
+            className="modify-doctor-cancel-btn"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+
+          <button
+            type="button"
+            className="modify-doctor-delete-btn"
+            onClick={handleDelete}
+          >
+            Delete Doctor
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModifyDoctorPage;
