5e54be20ea5cdb93a192ce3798149cea6a65a43d
nicolaics <nicolai.christian.s@gmail.com>
2024-09-28 16:03:57 +0900

    add parse start date and end date function

--- utils/utils.go
+++ utils/utils.go
@@ -59,3 +59,27 @@ func ParseDate(dateStr string) (*time.Time, error) {
 
 	return &date, nil
 }
+
+func ParseStartDate(dateStr string) (*time.Time, error) {
+	dateFormat := "2006-01-02 -0700MST"
+	date, err := time.Parse(dateFormat, dateStr)
+	if err != nil {
+		return nil, err
+	}
+
+	date = time.Date(date.Year(), date.Month(), date.Day(), 0, 0, 0, 0, date.Location())
+
+	return &date, nil
+}
+
+func ParseEndDate(dateStr string) (*time.Time, error) {
+	dateFormat := "2006-01-02 -0700MST"
+	date, err := time.Parse(dateFormat, dateStr)
+	if err != nil {
+		return nil, err
+	}
+
+	date = time.Date(date.Year(), date.Month(), date.Day(), 23, 59, 59, 999999999, date.Location())
+
+	return &date, nil
+}
