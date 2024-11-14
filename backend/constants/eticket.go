ccf6011cee253c9f3d95609328de17ac5b5df300
nicolaics <nicolai.christian.s@gmail.com>
2024-11-14 22:52:30 +1100

    add constants for 7x5 etix

--- backend/constants/eticket.go
+++ backend/constants/eticket.go
@@ -1,12 +1,22 @@
 package constants
 
 // measurement in cm
+const ETIX_MARGIN = 0.1
+
+// Etix 7x4
 const ETIX_7X4_WIDTH = 4
 const ETIX_7X4_HEIGHT = 7
 
 const ETIX_7X4_STD_CELL_HEIGHT = 0.6
 const ETIX_7X4_MAX_NAME_LEN = 25
 
-const ETIX_MARGIN = 0.1
-
 const ETIX_7X4_STD_FONT_SZ = 9
+
+// Etix 7x5
+const ETIX_7X5_WIDTH = 5
+const ETIX_7X5_HEIGHT = 7
+
+const ETIX_7X5_STD_CELL_HEIGHT = 0.7
+const ETIX_7X5_MAX_NAME_LEN = 25
+
+const ETIX_7X5_STD_FONT_SZ = 10
