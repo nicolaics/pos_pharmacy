2c168793f0f1e668c8b517171d2eaa2d9c7d2791
nicolaics <nicolai.christian.s@gmail.com>
2024-11-26 23:12:25 +0900

    return file name

--- backend/logger/WriteLog.go
+++ backend/logger/WriteLog.go
@@ -11,10 +11,10 @@ import (
 logType = ["delete", "modify"]
 logDataType = ["user", "invoice", "prescription", etc]
 */
-func WriteLog(logType string, logDataType string, userName string, dataId int, deletedData any) error {
+func WriteLog(logType string, logDataType string, userName string, dataId int, deletedData any) (string, error) {
 	logFolder := fmt.Sprintf("static/log/%s/%s", logType, logDataType)
 	if err := os.MkdirAll(logFolder, 0755); err != nil {
-		return err
+		return "", err
 	}
 
 	currentDate := time.Now().Format("060102-1504") // YYMMDD 형식
@@ -24,21 +24,21 @@ func WriteLog(logType string, logDataType string, userName string, dataId int, d
 	// 로그 파일 열기
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer file.Close()
 
 	// JSON으로 변환
 	jsonData, err := json.Marshal(deletedData)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	// 파일에 로그 기록
 	_, err = file.WriteString(fmt.Sprintf("%s\n", jsonData))
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	return nil
+	return fileName, nil
 }
