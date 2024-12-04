c3e7e02c8f219867bfc08a2ac18f66db164fc3f4
nicolaics <nicolai.christian.s@gmail.com>
2024-12-05 01:04:44 +0900

    add write xml file

---
+++ backend/utils/export/xml.go
@@ -0,0 +1,38 @@
+package export
+
+import (
+	"encoding/xml"
+	"os"
+)
+
+func CreateXmlData(fileName string, data any) (string, error) {
+	directory := "static/export/xml/"
+	if err := os.MkdirAll(directory, 0744); err != nil {
+		return "", err
+	}
+
+	filePath := directory + fileName
+
+	xmlFile, err := os.Create(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer xmlFile.Close()
+	
+	xmlFile.WriteString(xml.Header)
+
+	// output, err := xml.MarshalIndent(data, "", "\t")
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	encoder := xml.NewEncoder(xmlFile)
+	encoder.Indent("", "\t")
+	
+	err = encoder.Encode(&data)
+	if err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
