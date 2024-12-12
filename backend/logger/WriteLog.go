399a6cc4ab7ba7e85aa833d544ddc3a1e7b8fbf2
nicolaics <nicolai.christian.s@gmail.com>
2024-12-12 23:14:39 +0900

    fix logging

--- backend/logger/WriteLog.go
+++ backend/logger/WriteLog.go
@@ -3,8 +3,11 @@ package logger
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"time"
+
+	"github.com/nicolaics/pharmacon/utils"
 )
 
 /*
@@ -43,32 +46,42 @@ func WriteServerLog(logType string, logDataType string, userName string, dataId 
 	return nil
 }
 
-func WriteServerErrorLog(errorMsg any) (string, error) {
-	logFolder := "static/log/error"
+func WriteServerErrorLog(routes string, userId int, data any, errorMsg error) (string, error) {
+	log.Println(errorMsg)
+
+	logFolder := fmt.Sprintf("static/log/error/%s", time.Now().Format("2006-01-02"))
 	if err := os.MkdirAll(logFolder, 0755); err != nil {
 		return "", err
 	}
 
-	currentDate := time.Now().Format("060102-T-150405") // YYMMDD 형식
-
-	fileName := fmt.Sprintf("%s/%s.log", logFolder, currentDate)
+	currentDate := time.Now().Format("060102-150405") // YYMMDD-HHmmss
+	fileName := fmt.Sprintf("%s-%s", currentDate, utils.GenerateRandomCodeAlphanumeric(6))
+	filePath := fmt.Sprintf("%s/%s.log", logFolder, fileName)
 
 	// 로그 파일 열기
-	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return "", err
 	}
 	defer file.Close()
 
-	// JSON으로 변환
-	jsonData, err := json.Marshal(errorMsg)
-	if err != nil {
-		return "", err
+	var jsonData []byte
+
+	if data != nil {
+		jsonData, _ = json.Marshal(data)
 	}
 
-	// 파일에 로그 기록
-	_, err = file.WriteString(fmt.Sprintf("%s\n", jsonData))
+	// store the data into the file
+	msg := fmt.Sprintf("[Error %s]\n", time.Now().Format("2006/01/02 15:04:05"))
+	msg += fmt.Sprintf("Routes: %s\n", routes)
+	msg += fmt.Sprintf("User: %d\n", userId)
+	msg += fmt.Sprintf("Additional Data: %s\n\n", string(jsonData))
+	msg += fmt.Sprintf("%v\n", errorMsg)
+	msg += "\n-----------------------------------------------------------------------------------------------\n\n"
+
+	_, err = file.WriteString(msg)
 	if err != nil {
+		log.Printf("error write string: %v", err)
 		return "", err
 	}
 
